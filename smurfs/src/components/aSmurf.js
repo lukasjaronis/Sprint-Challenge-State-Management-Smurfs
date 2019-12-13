23190de4ac6e797d8e425ca35fb3ad9de8704551
lukasjaronis <jaronisluke13@gmail.com>
2019-12-13 11:14:51 -0500

    aSmurf template done

---
+++ smurfs/src/components/aSmurf.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const aSmurf = props => {
+
+    return(
+        <div>
+        <h1>name</h1>
+        <span>age</span>
+        <span>Height</span>
+        </div>
+    )
+}
+
+export default aSmurf;
